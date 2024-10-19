e971189b66b4fed78e7a54b2889be06ec6599218
justamitsaha <justamitsaha@gmail.com>
2024-10-19 12:11:58 +0530

    Local set up

---
+++ implementation/cheetsheet.sh
@@ -0,0 +1,10 @@
+kubectl config get-contexts
+kubectl config use-context docker-desktop
+
+kubectl get nodes
+kubectl get nodes -o wide
+
+kubectl get pods
+
+kubectl apply -f .\local
+kubectl delete -f .\local
--- kubernetes-fundamentals-master/01-Kubernetes-Architecture/README.md
+++ kubernetes-fundamentals-master/01-Kubernetes-Architecture/README.md
@@ -1,5 +1,9 @@
 # Kubernetes Architecture
 
+kubectl config get-contexts
+
+kubectl config use-context docker-desktop
+
 ## Step-01: Why Kubernetes?
 
 ## Step-02: Kubernetes Architecture
